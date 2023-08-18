e66a27329f5a2578a83f8fb52abac905976ec72a
Şule Duman <sduman_816@hotmail.com>
2023-08-19 01:10:00 +0300

    Initial commit

---
+++ lib/global_key_kullanimi.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inputs/sayac_state.dart';
+
+class GlobalKeyKullanimi extends StatelessWidget {
+  GlobalKeyKullanimi({Key? key}) : super(key: key);
+  final sayacWidgetKey = GlobalKey<SayacWidgetState>();
+
+  @override
+  Widget build(BuildContext context) {
+    print('1');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Global Key Kullanımı'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Butona basılma miktarı:'),
+            SayacWidget(
+              key: sayacWidgetKey,
+            ),
+            Text(sayacWidgetKey.currentState?.sayac.toString() ?? '0'),
+
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          sayacWidgetKey.currentState!.arttir();
+        },
+      ),
+    );
+  }
+}
+
